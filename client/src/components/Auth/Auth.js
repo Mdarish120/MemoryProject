38bb5df6d6192c10e029c2a9f20c6a06ff8f7c3a
Mohd Arish <arishmd768@gmail>
2023-09-12 01:59:58 +0530

    add client id

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -79,7 +79,7 @@ const SignUp = () => {
             { isSignup ? 'Sign Up' : 'Sign In' }
           </Button>
           <GoogleLogin
-            clientId="564033717568-bu2nr1l9h31bhk9bff4pqbenvvoju3oq.apps.googleusercontent.com"
+            clientId="316651532771-a1d4ib0g355dogg58nhlp7t3a41gcqhn.apps.googleusercontent.com"
             render={(renderProps) => (
               <Button className={classes.googleButton} color="primary" fullWidth onClick={renderProps.onClick} disabled={renderProps.disabled} startIcon={<Icon />} variant="contained">
                 Google Sign In
